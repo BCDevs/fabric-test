724ce705e33ee28d80c2f17e44cf222fcb94b2ee
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-09-09 08:04:44 +0530

    Create invoke2.js

---
+++ client/javascript/invoke2.js
@@ -0,0 +1,50 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const { FileSystemWallet, Gateway } = require('fabric-network');
+const path = require('path');
+
+const ccpPath = path.resolve(__dirname, '..', '..', 'first-network', 'connection-org2.json');
+
+async function main() {
+    try {
+
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = new FileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the user.
+        const userExists = await wallet.exists('user2');
+        if (!userExists) {
+            console.log('An identity for the user "user2" does not exist in the wallet');
+            console.log('Run the registerUser2.js application before retrying');
+            return;
+        }
+
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccpPath, { wallet, identity: 'user2', discovery: { enabled: true, asLocalhost: true } });
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('mychannel');
+
+        // Get the contract from the network.
+        const contract = network.getContract('ecom');
+
+         await contract.submitTransaction('addProduct', 'Item5', 'Laptops', 'MSFTP3', 'Microsoft Surface Pro3', '487','7');
+        console.log('Transaction has been submitted');
+
+        // Disconnect from the gateway.
+        await gateway.disconnect();
+
+    } catch (error) {
+        console.error(`Failed to submit transaction: ${error}`);
+        process.exit(1);
+    }
+}
+
+main();
